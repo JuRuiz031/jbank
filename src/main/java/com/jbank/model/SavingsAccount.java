f3849cbf8d7ee6437c58ac842c4a884a1285a37e
Juan Ruiz <77316037+JuRuiz031@users.noreply.github.com>
2025-12-08 09:50:11 -0600

    t

---
+++ src/main/java/com/jbank/model/SavingsAccount.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.jbank.model;
+
+/**
+ *
+ * @author juanfruiz
+ */
+public class SavingsAccount extends AbstractAccount implements Depositable, Withdrawable {
+    private double interestRate;
+    private int withdrawalLimit;
+    private int withdrawalCounter;
+
+    public SavingsAccount(long customerID, double initialDeposit, String accountName,
+            double interestRate, int withdrawalLimit) {
+        super(customerID, initialDeposit, accountName);
+        // Input validation
+        if(interestRate < 0) {
+            throw new IllegalArgumentException("Interest rate cannot be negative.");
+        }
+        if(withdrawalLimit < 0) {
+            throw new IllegalArgumentException("Withdrawal limit cannot be negative.");
+        }
+        if(withdrawalCounter < 0) {
+            throw new IllegalArgumentException("Withdrawal counter cannot be negative.");
+        }
+
+        this.interestRate = interestRate;
+        this.withdrawalLimit = withdrawalLimit;
+        this.withdrawalCounter = 0;
+    }
+
+    // Interface Implementations
+    @Override
+    public void deposit (double depositAmount) {
+        if(depositAmount <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be positive.");
+        }
+        double newBalance = this.getBalance() + depositAmount;
+        this.updateBalance(newBalance);
+    }
+
+    @Override
+    public void withdraw (double withdrawAmount) {
+        if(withdrawAmount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be positive.");
+        }
+        if (withdrawalCounter >= withdrawalLimit) {
+            throw new IllegalArgumentException("Withdrawal limit reached.");
+        }
+        double newBalance = this.getBalance() - withdrawAmount;
+        if (newBalance < 0) {
+            throw new IllegalArgumentException("Insufficient funds for withdrawal.");
+        }
+        this.updateBalance(newBalance);
+        withdrawalCounter++;
+    }
+
+    // Mutators
+    public void resetWithdrawalCounter() {
+        this.withdrawalCounter = 0;
+    }
+    public void setInterestRate(double interestRate) {
+        if(interestRate < 0) {
+            throw new IllegalArgumentException("Interest rate cannot be negative.");
+        }
+        this.interestRate = interestRate;
+    }
+    public void updateWithdrawalLimit(int withdrawalLimit) {
+        if(withdrawalLimit < 0) {
+            throw new IllegalArgumentException("Withdrawal limit cannot be negative.");
+        }
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+
+    // Getter
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public int getWithdrawalLimit() {
+        return withdrawalLimit;
+    }
+
+    public int getWithdrawalCounter() {
+        return withdrawalCounter;
+    }
+
+}
