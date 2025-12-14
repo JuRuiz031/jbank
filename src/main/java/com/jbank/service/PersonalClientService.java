5a22074b077de18f52de13a38200b8bafa79a616
JFRuiz <77316037+JuRuiz031@users.noreply.github.com>
2025-12-13 22:37:38 -0600

    Wrote ServiceInterface, created PersonalClientService

---
+++ src/main/java/com/jbank/service/PersonalClientService.java
@@ -0,0 +1,10 @@
+
+package com.jbank.service;
+
+/**
+ *
+ * @author juanf
+ */
+public class PersonalClientService {
+
+}
---
+++ src/main/java/com/jbank/service/ServiceInterface.java
@@ -0,0 +1,21 @@
+package com.jbank.service;
+
+import java.util.List;
+import java.util.Optional;
+/**
+ *
+ * @author juanf
+ */
+public interface ServiceInterface<T,U> {
+
+    // CRUD operations
+    Integer createEntity(T entity);
+    Optional<T> getEntityById(Integer id);
+    List<T> getAllEntities();
+    T updateEntity(Integer id, T newEntity);
+
+    // Conversion methods
+    Optional<U> convertEntityToModel(T entity);
+    Optional<T> convertModelToEntity(U model);
+    Optional<U> getModelById(Integer id);
+}
